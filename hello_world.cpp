b3a91e6906672e58dc4b639aee97e2634096e763
LevushkaStarikov <lev-starikov@mail.ru>
2022-03-18 20:23:49 +0300

    Напортачил

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 #include <string>
 
-
-int main()
+int
+main()
 {
-std::string name;
-//коментарий
-    std::cin >> name;
-   std::cout << "Hello, world from " << name;
-    return 0;
+  std::string name;
+  //коментарий
+  std::cin >> name;
+  std::cout << "Hello, world from " << name;
+  return 0;
 }
